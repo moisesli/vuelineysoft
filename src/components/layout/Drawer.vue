b5ad3667d739efa88c4f31416ce55521225ddf14
moises <elnaufrago2009@gmail.com>
2019-10-31 13:43:37 -0500

    base

--- src/components/layout/Drawer.vue
+++ src/components/layout/Drawer.vue
@@ -1,8 +1,41 @@
 <template>
-  <v-navigation-drawer
-      v-model="drawer"
-      app
-  >
+  <v-navigation-drawer class="blue lighten-5" v-model="drawer" app>
+
+    <v-list class="pa-0">
+      
+      <v-list-tile avatar tag="div">
+        
+        <v-list-tile-avatar>
+          <img src="../../assets/avatar0.png">
+        </v-list-tile-avatar>
+        <!-- <v-list-tile-content>
+          <v-list-tile-title>{{user.firstName}}{{user.lastName}}</v-list-tile-title>
+        </v-list-tile-content>
+        <v-spacer></v-spacer>
+        <v-list-tile-action style="min-width:30px;">
+          <v-menu bottom right offset-y origin="bottom right" transition="v-slide-y-transition">
+            <v-btn icon light slot="activator">
+              <v-icon>more_vert</v-icon>
+            </v-btn>
+            <v-list>
+              <v-list-tile v-for="item in userMenus" :key="item.title" value="true" :to="item.link" router>
+                <v-list-tile-title v-text="item.title"></v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
+
+        </v-list-tile-action>
+        <v-list-tile-action style="min-width:30px;">
+          <v-btn icon @click.native.stop="mini = !mini">
+            <v-icon>chevron_left</v-icon>
+          </v-btn>
+        </v-list-tile-action> -->
+
+      </v-list-tile>
+
+    </v-list>
+
+
     <v-list dense>
       <v-list-item @click="">
         <v-list-item-action>
